3673b0ad9cb9ae0e5d6f56d9dab1552ed989fae3
2tomas <tmsrch@gmail.com>
2025-12-13 17:11:02 -0300

    fix lea

--- src/app/componentes/credencial/credencial.component.ts
+++ src/app/componentes/credencial/credencial.component.ts
@@ -47,7 +47,7 @@ export class CredencialComponent {
   }
 
   generateToken(): number {
-    return Math.floor(1000000000 + Math.random() * 9000000000); // 10 dígitos
+    return Math.floor(10000 + Math.random() * 90000);
   }
 
     getQrData() {
